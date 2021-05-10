dcf58e97abc042f07e96ca729d0d46fc73ef10bd
orr1nuycruz <orr1n@hotmail.com>
2021-05-10 03:16:35 -0400

    adjust video display for calling

--- public/scripts.js
+++ public/scripts.js
@@ -1,5 +1,9 @@
 const socket = io("/")
 const videoGrid = document.getElementById("gridview-video")
+const mainVideo = document.getElementById("main-video")
+const allPage = document.getElementById("allpage")
+const firstDivVideo = document.createElement("div")
+firstDivVideo.className = "firstOuter-video"
 
 const myPeer = new Peer(undefined, {
     host: "/",
@@ -14,19 +18,37 @@ navigator.mediaDevices.getUserMedia({
     video: true,
     audio: true
 }).then((stream) => {
-    addVideoStream(myVideo, stream);
+    // addVideoStream(myVideo, stream);
+    myVideo.srcObject = stream;
+    myVideo.addEventListener("loadedmetadata", () => {
+        myVideo.play()
+    })
+    mainVideo.append(firstDivVideo)
+    firstDivVideo.append(myVideo)
 
     myPeer.on("call", (call) => {
         call.answer(stream);
-        const video = document.createElement("video");
+        const video = document.createElement("video")
         call.on("stream", (userVideoStream) => {
             addVideoStream(video, userVideoStream)
+            // video.srcObject = stream;
+            // video.addEventListener("loadedmetadata", () => {
+            //     video.play()
+            // })
+            // videoGrid.append(firstDivVideo)
+            // firstDivVideo.append(video)
+            console.log(JSON.stringify(peers))
         });
     });
     socket.on("user-connected", (userId) => {
         connectToNewUser(userId, stream)
+        // console.log(userId)
     });
 });
+
+myPeer.on("open", (id) => {
+    socket.emit("join-room", ROOM_ID, id)
+})
 
 
 socket.on("user-disconnected", (userId) => {
@@ -34,9 +56,6 @@ socket.on("user-disconnected", (userId) => {
         peers[userId].closer();
 })
 
-myPeer.on("open", (id) => {
-    socket.emit("join-room", ROOM_ID, id)
-})
 
 
 function connectToNewUser(userId, stream) {
@@ -45,19 +64,32 @@ function connectToNewUser(userId, stream) {
     call.on("stream", (userVideoStream) => {
         addVideoStream(video, userVideoStream)
     })
-
     call.on("close", () => {
         video.remove();
     })
-
     peers[userId] = call;
-
-
 }
+
 function addVideoStream(video, stream) {
+    const divVideo = document.createElement("div")
+    divVideo.className = "outer-video"
     video.srcObject = stream;
     video.addEventListener("loadedmetadata", () => {
         video.play()
     })
+    // videoGrid.append(divVideo)
+    // divVideo.append(video)
+    videoGrid.append(video)
+}
+
+function addExtraVideoStream(video, stream) {
+    const divVideo = document.createElement("div")
+    divVideo.className = "outer-video"
+    video.srcObject = stream;
+    video.addEventListener("loadedmetadata", () => {
+        video.play()
+    })
+    // videoGrid.append(divVideo)
+    // divVideo.append(video)
     videoGrid.append(video)
 }
--- server.js
+++ server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
+let path = require('path');
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const {v4 : uuidv4} = require("uuid");
 
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(express.static('styles'));
 
 app.use(cors({ origin: "http://localhost:3000" }));
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -25,6 +27,7 @@ io.on('connection', (socket) => {
     socket.on("join-room", (roomId, userId) =>{
         socket.join(roomId);
         socket.to(roomId).broadcast.emit("user-connected", userId);
+        console.log(userId)
 
         socket.on("disconnected", () =>{
             socket.to(roomId).broadcast.emit("user-disconnected", userId);
